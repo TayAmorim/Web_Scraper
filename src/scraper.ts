82181babfce4db832263d6cddc59d19eac686312
TayAmorim <amorim.tayanna@gmail.com>
2024-04-18 08:59:46 -0300

    :goal_net: fix: erro compilação ts

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,19 +1,30 @@
 const puppeteer = require('puppeteer');
 
-const bestsellersv = async () => {
+
+type ProductProps = {
+    categoria: string,
+    name: string
+}
+
+
+const bestseller = async () => {
+    
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     await page.goto("https://www.amazon.com.br/bestsellers)")
 
-    await page.waitForSelector('.a-section a-spacing-large')
 
-    const product = await page.$$eval('.a-carousel-card', (items) => {
-        const bestsellers: string[] = []
+    const product = await page.$$eval('[data-a-carousel-options]', (items: any[]) => {
+        const bestsellers: ProductProps[] = []
         items.map((item) => {
-            const name = item?.querySelector('a span div')?.innerText.trim()
+            //item.querySelectorAll('card').forEach((element) => {})
+
+            const categoria = item?.querySelector('.a-carousel-heading')?.innerText.trim()
+            const name = item?.querySelector('.a-carousel-card a span div')?.innerText.trim()
+            
             if (name) {
-                bestsellers.push(name)
+                bestsellers.push({categoria, name})
             }
             
             
@@ -21,8 +32,11 @@ const bestsellersv = async () => {
         return bestsellers
     })
     console.log(product);
+
+
+    await browser.close();
     
 }
 
-bestsellersv(); 
+bestseller(); 
 
