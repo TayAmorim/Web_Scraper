45a9f39d99ebaecae86e46d2a72106c17b222650
TayAmorim <amorim.tayanna@gmail.com>
2024-04-21 11:04:32 -0300

    :boom: fix: corrigingo indentação

--- src/handler.ts
+++ src/handler.ts
@@ -1,55 +1,44 @@
-import { bestseller } from "./scraper"
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
-import {
-  DynamoDBDocumentClient,
-  PutCommand,
-} from "@aws-sdk/lib-dynamodb";
+import { bestseller } from "./scraper";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 
-
-
-
-const dbClient = new DynamoDBClient({region: 'us-east-1'});
+const dbClient = new DynamoDBClient({ region: "sa-east-1" });
 const dynamo = DynamoDBDocumentClient.from(dbClient);
-const nameTable = 'products'
+const nameTable = "products";
 
-export const handlerScrapperBd = async () =>  {
+export const handlerScrapperBd = async () => {
   try {
     const listProduct = await bestseller();
-    
+
     if (listProduct != null && listProduct.length > 0) {
       for (let product of listProduct) {
         const params = {
-            TableName: nameTable,
-            Item: {
-              ...product
-            }
-        }
-        await dynamo.send(new PutCommand(params))
-        
+          TableName: nameTable,
+          Item: {
+            ...product,
+          },
+        };
+        await dynamo.send(new PutCommand(params));
       }
       return {
         statusCode: 200,
-        body: JSON.stringify(
-          {
-            message: JSON.stringify(listProduct),
-          },
-        ),
+        body: JSON.stringify({
+          message: JSON.stringify(listProduct),
+        }),
       };
     }
-    
+
     return {
       statusCode: 404,
-      body: JSON.stringify(
-        {
-          message: `Lista vazia ${listProduct}`,
-        },
-      ),
+      body: JSON.stringify({
+        message: `Lista vazia ${listProduct}`,
+      }),
     };
-    
   } catch (error) {
-    return {status: 500, message: 'Erro ao cadastrar no banco de dados', error}
-    
-  } 
-}
-
-
+    return {
+      status: 500,
+      message: "Erro ao cadastrar no banco de dados",
+      error,
+    };
+  }
+};
